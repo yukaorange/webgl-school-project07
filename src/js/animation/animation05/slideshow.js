d9abc2e38e1e3fbd2d9a5d1ff8df097788436da2
yukaorange <takaokar19911013t@gmail.com>
2023-08-09 23:10:28 +0900

    05

---
+++ src/js/animation/animation05/slideshow.js
@@ -0,0 +1,188 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export class Slideshow {
+  constructor(element) {
+    this.interval = 6000;
+    this.crearance = 1;
+    this.elements = {
+      hero: document.querySelector(".slideshow"),
+      container: element,
+      svg: element.querySelector("svg"),
+      images: Array.from(element.querySelectorAll("svg image")),
+      currentImage: null,
+      mask: element.querySelector("svg clipPath"),
+      rectangles: [],
+    };
+    this.width = parseInt(this.elements.svg.getAttribute("width"), 10);
+    this.height = parseInt(this.elements.svg.getAttribute("height"), 10);
+    this.currentIndex = 0;
+    this.totalImages = this.elements.images.length;
+    this.totalMasks = parseInt(
+      this.elements.container.dataset.totalMasks || 10,
+      10
+    );
+    this.maskId = this.elements.mask.getAttribute("id");
+    this.isCreated = false;
+    this.isActive = false;
+    this.isChanging = false;
+    this.scrollTrigger = null;
+    this.timer = null;
+
+    console.log(this.elements);
+  }
+
+  create() {
+    if (!this.isCreated && this.elements.images.length) {
+      this.isCreated = true;
+      this.updateSize();
+      this.createRectangles();
+      this.slideTo(0);
+      this.elements.container.classList.add("is-created");
+    }
+  }
+
+  updateSize() {
+    const image = this.elements.images[0],
+      imageWidth = parseInt(image.getAttribute("width"), 10),
+      imageHeight = parseInt(image.getAttribute("height"), 10);
+    this.width = imageWidth;
+    this.height = imageHeight;
+    this.elements.svg.setAttribute("width", imageWidth);
+    this.elements.svg.setAttribute("height", imageHeight);
+    this.elements.svg.setAttribute(
+      "viewBox",
+      `0 0 ${imageWidth} ${imageHeight}`
+    );
+  }
+
+  createRectangles() {
+    const rectangleHeight = this.height / this.totalMasks;
+    for (let i = 0; i < this.totalMasks; i++) {
+      const rect = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "rect"
+      );
+      rect.setAttribute("x", -1);
+      rect.setAttribute("y", i * rectangleHeight - 1);
+      rect.setAttribute("width", this.width + 2);
+      rect.setAttribute("height", rectangleHeight + 2);
+      this.elements.mask.append(rect);
+    }
+    this.elements.rectangles = Array.from(
+      this.elements.mask.querySelectorAll("rect")
+    );
+  }
+
+  waitForAnimation(duration) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, duration);
+    });
+  }
+
+  async slideTo(targetIndex) {
+    if (this.isChanging) return;
+    this.isChanging = true;
+    const targetImage = this.elements.images[targetIndex];
+    this.elements.svg.prepend(targetImage);
+    targetImage.setAttribute("opacity", 1);
+    // await this.waitForAnimation(16);
+    if (this.elements.currentImage) {
+      gsap.set(this.elements.rectangles, { x: 0 });
+      // await this.waitForAnimation(16);
+      this.elements.currentImage.setAttribute(
+        "clip-path",
+        `url(#${this.maskId})`
+      );
+      gsap.fromTo(
+        targetImage,
+        {
+          scale: 1.2,
+          transformOrigin: "center center",
+        },
+        {
+          scale: 1,
+          duration: 3.2,
+          ease: "power4.out",
+        }
+      );
+      await Promise.all(
+        this.elements.rectangles.map((rectangle, i) => {
+          const distance = i % 2 === 0 ? this.width + 1 : -1 * (this.width + 1);
+          return gsap
+            .to(rectangle, {
+              x: distance,
+              duration: 1,
+              ease: "expo.inOut",
+              delay: 0.08 * i,
+            })
+            .then();
+        })
+      );
+      this.elements.currentImage.setAttribute("opacity", 0);
+      this.elements.currentImage.setAttribute("clip-path", "");
+    }
+    this.isChanging = false;
+    this.currentIndex = targetIndex;
+    this.elements.currentImage = targetImage;
+  }
+
+  next() {
+    const nextIndex =
+      this.currentIndex === this.totalImages - 1 ? 0 : this.currentIndex + 1;
+    this.slideTo(nextIndex);
+  }
+
+  on() {
+    this.scrollTrigger = ScrollTrigger.create({
+      trigger: this.elements.hero,
+      start: "top bottom",
+      onEnter: () => {
+        this.start();
+      },
+      onEnterBack: () => {
+        this.start();
+      },
+      onLeave: () => {
+        this.stop();
+      },
+      onLeaveBack: () => {
+        this.stop();
+      },
+    });
+  }
+
+  off() {
+    this.scrollTrigger && this.scrollTrigger.kill();
+    this.stop();
+  }
+
+  loop(customInterval) {
+    const interval =
+      customInterval !== undefined ? customInterval : this.interval;
+    if (this.timer !== null) clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this.timer = null;
+      if (this.isActive) {
+        this.next();
+        this.loop();
+      }
+    }, interval);
+  }
+
+  start() {
+    if (!this.isActive) {
+      this.isActive = true;
+      this.loop(800);
+    }
+  }
+
+  stop() {
+    if (this.isActive) {
+      this.isActive = false;
+    }
+  }
+}
--- src/js/barbaConfig.js
+++ src/js/barbaConfig.js
@@ -3,68 +3,74 @@ import gsap from "gsap";
 import { ScrollToPlugin } from "gsap/all";
 gsap.registerPlugin(ScrollToPlugin);
 
-initHeight();
-function initHeight() {
-  const barbaContainerHeight =
-    document.querySelector(".barba-container").clientHeight;
+const barbaContainer = document.querySelector(".barba-container");
+
+function initHeight(barbaContainer) {
+  const barbaContainerHeight = barbaContainer.clientHeight;
   const barbaWrapper = document.querySelector(".barba-wrapper");
   barbaWrapper.style.height = barbaContainerHeight + "px";
 
   window.addEventListener("resize", () => {
-    const barbaContainerHeight =
-      document.querySelector(".barba-container").clientHeight;
+    const barbaContainerHeight = barbaContainer.clientHeight;
     const barbaWrapper = document.querySelector(".barba-wrapper");
     barbaWrapper.style.height = barbaContainerHeight + "px";
   });
 }
 
-barba.init({
-  transitions: [
-    {
-      sync: true,
-      name: "opacity-transition",
-      leave(data) {
-        const tl = gsap.timeline();
+if (barbaContainer) {
+  initHeight(barbaContainer);
+  
+  barba.init({
+    transitions: [
+      {
+        sync: true,
+        name: "opacity-transition",
+        leave(data) {
+          const tl = gsap.timeline();
 
-        tl.to(data.current.container, {
-          duration: 1.5,
-          clipPath: "polygon(0 0, 0% 0, 0% 100%, 0% 100%)",
-        });
-
-        tl.to(
-          data.current.container,
-          {
+          tl.to(data.current.container, {
             duration: 1.5,
-            xPercent: -0,
-          },
-          "0.5"
-        );
-        return tl;
-      },
-      enter(data) {
-        const tl = gsap.timeline();
-        tl.from(data.next.container, {
-          duration: 1.5,
-          clipPath: " polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
-          onUpdate: () => {
-            data.next.container.style.transformOrigin = "100% 100%";
-          },
-          onComplete: () => {
-            const barbaWrapper = document.querySelector(".barba-wrapper");
-            barbaWrapper.style.height = data.next.container.clientHeight + "px";
-          },
-        });
-        tl.to(
-          window,
-          {
-            duration: 0.6,
-            scrollTo: 0,
-          },
-          "0.8"
-        );
+            clipPath: "polygon(0 0, 0% 0, 0% 100%, 0% 100%)",
+          });
 
-        return tl;
+          tl.to(
+            data.current.container,
+            {
+              duration: 1.5,
+              xPercent: -0,
+            },
+            "0.5"
+          );
+          return tl;
+        },
+        enter(data) {
+          const tl = gsap.timeline();
+          tl.from(data.next.container, {
+            duration: 1.5,
+            clipPath: " polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
+            onUpdate: () => {
+              data.next.container.style.transformOrigin = "100% 100%";
+            },
+            onComplete: () => {
+              const barbaWrapper = document.querySelector(".barba-wrapper");
+              barbaWrapper.style.height =
+                data.next.container.clientHeight + "px";
+            },
+          });
+          tl.to(
+            window,
+            {
+              duration: 0.6,
+              scrollTo: 0,
+            },
+            "0.8"
+          );
+
+          return tl;
+        },
       },
-    },
-  ],
-});
+    ],
+  });
+}
+
+//読み込んだコンテンツのスクロール位置をリセットしているようなテクニックは、bodyのスクロールを使わずに、containerなりでスクロール、つまりbodyはheight100vhにして、そしてcontainerはheight100%にしてoverflow:scrollにするという方法があります。このとき、スクロールプラグインはlenis。参考サイトhttps://www.bowte.jp/about/
