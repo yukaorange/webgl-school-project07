6b2f2b79314e6570cc29bb96e40f71d4234aa982
yukaorange <takaoka.webdesigner@gmail.com>
2023-10-12 09:27:43 +0900

    completed

--- src/js/webgl/main.js
+++ src/js/webgl/main.js
@@ -2,6 +2,7 @@
 
 // import * as DAT from 'dat.gui'
 import { mat4 } from 'gl-matrix'
+import { ScrollAmountHandler } from 'src/js/utils/ScrollHandler.js'
 import { Camera } from './common/js/Camera.js'
 import { Clock } from './common/js/Clock.js'
 import { Controls } from './common/js/Controls.js'
@@ -13,7 +14,6 @@ import { utils } from './common/js/utils.js'
 // import { Axis } from './common/js/Axis'
 import fragmentShader from './shader/fragment.glsl'
 import vertexShader from './shader/vertex.glsl'
-import { ScrollAmountHandler } from '/src/js/utils/ScrollHandler.js'
 
 let gl,
   scene,
